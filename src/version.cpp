5fef7980c18c5c9fb85f0109ed81708b773c2bcb
Zettelkasten <37848177+zettel-kasten@users.noreply.github.com>
2018-06-07 12:57:14 +0200

    new CLIENT_VERSION_BUILD
    new CLIENT_VERSION_BUILD and leave GIT ARCHIVE date and hash open to be filled in by builder

--- src/version.cpp
+++ src/version.cpp
@@ -36,8 +36,8 @@ const std::string CLIENT_NAME("Satoshi");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "dba4d2b"
-#    define GIT_COMMIT_DATE "Sat, 24 Jan 2015 01:02:16 +0300"
+#    define GIT_COMMIT_ID ""
+#    define GIT_COMMIT_DATE ""
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
