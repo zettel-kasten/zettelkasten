1e13de2e7bc603e39329c30f9bfe4876e2039abc
Zettelkasten <37848177+zettel-kasten@users.noreply.github.com>
2018-06-22 16:19:07 +0200

    conditional fix for specific linux wallet crash
    conditional fix for specific linux wallet crash, see issue 8:
    https://github.com/zettel-kasten/zettelkasten/issues/8

--- src/qt/miningpage.cpp
+++ src/qt/miningpage.cpp
@@ -80,6 +80,8 @@ void MiningPage::restartMining(bool fGenerate)
     mapArgs["-genproclimit"] = QString("%1").arg(nThreads).toUtf8().data();
 
     // unlock wallet before mining
+
+#ifndef __linux__
     if (fGenerate && !hasMiningprivkey && !unlockContext.get())
     {
         this->unlockContext.reset(new WalletModel::UnlockContext(model->requestUnlock()));
@@ -89,6 +91,7 @@ void MiningPage::restartMining(bool fGenerate)
             return;
         }
     }
+#endif
 
     json_spirit::Array Args;
     Args.push_back(fGenerate);
