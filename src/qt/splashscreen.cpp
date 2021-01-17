70cc3b527b8014c55bce0c96a1e2020b120c153d
Zettelkasten <37848177+zettel-kasten@users.noreply.github.com>
2021-01-17 23:59:11 +0100

    center splashscreen correctly
    center splashscreen correctly (it was off before, too low)

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -16,30 +16,8 @@ SplashScreen::SplashScreen(QWidget *parent) :
     int screenHeight = rec.height();
 
     this->setWindowFlags(Qt::FramelessWindowHint);
-    this->setGeometry(screenWidth/2-200,screenHeight/2-12,400,260);
+    this->setGeometry(screenWidth/2-200,screenHeight/2-130,400,260);
 
-
-    //QPixmap bgPixmap(320,360);
-
-    /*QLinearGradient bgGradient(QPointF(0, 0), QPointF(screenWidth, 0));
-    bgGradient.setColorAt(0, QColor("#FEFEFE"));//#6c3d94"));
-    bgGradient.setColorAt(1, QColor("#FEFEFE"));//"#a13469"));
-    */
-    //#3c3c3b
-
- /*   QRect rect_linear(0,0,320,360);
-
-    QPainter *painter = new QPainter(&bgPixmap);
-    painter->fillRect(rect_linear, bgGradient);
-
-    painter->end();
-
-    bg = new QLabel(this);
-    bg->setPixmap(bgPixmap);
-
-
-    bg->setGeometry(0,0,320,360);
-*/
     splashImage = new QLabel(this);
     QPixmap newPixmap;
     newPixmap.load(":/images/splash");
