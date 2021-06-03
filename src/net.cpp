7332d6de1ddf705d841fb4b02151371ca3abc591
Zettelkasten <37848177+zettel-kasten@users.noreply.github.com>
2021-06-03 10:48:34 +0200

    replaced showip
    removed the no more existing showip.com with my own zettelkasten checkip subdomain.

--- src/net.cpp
+++ src/net.cpp
@@ -350,23 +350,19 @@ bool GetMyExternalIP(CNetAddr& ipRet)
     const char* pszGet;
     const char* pszKeyword;
 
-    for (int nLookup = 0; nLookup <= 1; nLookup++)
+    //for (int nLookup = 0; nLookup <= 1; nLookup++)
     for (int nHost = 1; nHost <= 2; nHost++)
     {
         // We should be phasing out our use of sites like these. If we need
         // replacements, we should ask for volunteers to put this simple
         // php file on their web server that prints the client IP:
         //  <?php echo $_SERVER["REMOTE_ADDR"]; ?>
+
         if (nHost == 1)
         {
-            addrConnect = CService("91.198.22.70", 80); // checkip.dyndns.org
-
-            if (nLookup == 1)
-            {
-                CService addrIP("checkip.dyndns.org", 80, true);
-                if (addrIP.IsValid())
-                    addrConnect = addrIP;
-            }
+            CService addrIP("checkip.dyndns.org", 80, true);
+            if (addrIP.IsValid())
+                addrConnect = addrIP;
 
             pszGet = "GET / HTTP/1.1\r\n"
                      "Host: checkip.dyndns.org\r\n"
@@ -376,24 +372,19 @@ bool GetMyExternalIP(CNetAddr& ipRet)
 
             pszKeyword = "Address:";
         }
-        else if (nHost == 2)
+        if (nHost == 2)
         {
-            addrConnect = CService("74.208.43.192", 80); // www.showmyip.com
-
-            if (nLookup == 1)
-            {
-                CService addrIP("www.showmyip.com", 80, true);
-                if (addrIP.IsValid())
-                    addrConnect = addrIP;
-            }
+            CService addrIP("checkip.zettelkasten.org", 80, true);
+            if (addrIP.IsValid())
+                addrConnect = addrIP;
 
             pszGet = "GET /simple/ HTTP/1.1\r\n"
-                     "Host: www.showmyip.com\r\n"
+                     "Host: checkip.zettelkasten.org\r\n"
                      "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)\r\n"
                      "Connection: close\r\n"
                      "\r\n";
 
-            pszKeyword = NULL; // Returns just IP address
+            pszKeyword = "Address:";
         }
 
         if (GetMyExternalIP2(addrConnect, pszGet, pszKeyword, ipRet))
